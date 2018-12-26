1ebfd820b0d8b587d047c72b0664741903584444
Quickzotick <solk010@gmail.com>
2018-12-26 13:25:17 +0300

    adaptive finished

--- app/js/myscripts.js
+++ app/js/myscripts.js
@@ -1,3 +1,10 @@
 $(document).ready(function() {
+	$(document).on('click', '.block-main-gallery-preview-img__item:not(.active)', function(){
+		let a = $(this).index();
 
+		$(this).siblings('.block-main-gallery-preview-img__item').removeClass('active');
+		$(this).addClass('active');
+
+		$(document).find('.block-main-gallery-big-img__item').removeClass('active').eq(a).addClass('active');
+	});
 });
